d74d21962a210847f72220325876491695ddbb54
MarkXado <79851214062@yandex.ru>
2020-01-03 23:43:45 +0300

    Admin django create

--- MySite/app/admin.py
+++ MySite/app/admin.py
@@ -3,5 +3,29 @@ from django.contrib import admin
 # Register your models here.
 from app.models import CarPicture, CarPictureInstance
 
-admin.site.register(CarPicture)
-admin.site.register(CarPictureInstance)
+
+# admin.site.register(CarPicture)
+# admin.site.register(CarPictureInstance)
+# Register the Admin classes for Book using the decorator
+class CarPictureInline(admin.TabularInline):
+    model = CarPictureInstance
+
+
+@admin.register(CarPicture)
+class CarPictureAdmin(admin.ModelAdmin):
+    list_display = ('image', 'title')
+    inlines = [CarPictureInline]
+
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(CarPictureInstance)
+class CarPictureInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('image', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
